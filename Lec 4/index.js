1263e3cda819ccaf1919e01c633e783445e244d0
Tushya Gupta <tushya2311.be23@chitkara.edu.in>
2025-07-14 15:30:21 +0530

    hi

---
+++ Lec 4/index.js
@@ -0,0 +1,72 @@
+// function buyproduct(product_name,cb){
+//     //do some asynchronous operation
+//     setTimeout(()=>{
+//         //all the operation completed
+//         console.log("all the IO operation is completed and order details are written in user data")
+//         cb();
+//     })
+// }
+
+// buyproduct("Iphone 15",function(){
+//     console.log("product is purchased")
+// })
+let product=[{
+    name:"samsung",
+    amount:70000,
+    quantity:10
+},
+{
+    name:"iPhone 16",
+    amount:100000,
+    quantity:0
+}
+]
+function buyproduct(product_name) {
+    return new Promise((resolve, reject) => {
+        // do some asynchronous operation
+        let isProduct = product.filter((p) => p.name == product_name)[0];
+        if (!isProduct) {
+            return reject("product is not available");
+        }
+        resolve(isProduct.amount);
+    });
+}
+let availableamount=10000000
+function deductbankamount(amount,cb){
+    return new Promise((resolve, reject) => {
+        // do some bank transactions
+        if (amount > availableamount) {
+            return reject("bank balance is low");
+        } else {
+            availableamount -= amount;
+            console.log(availableamount);
+            return resolve("amount deducted");
+        }
+    });
+}
+
+buyproduct("samsung",function(err,amount){
+    if(err) return console.log(err)
+    console.log(amount);
+    deductbankamount(amount).then((message) => {
+        console.log(message);
+    }).catch((err) => {
+        console.log(err);
+    });
+
+    buyproduct("samsung").then((amount) => {
+        console.log(amount);
+        return deductbankamount(amount);
+    }).then((message) => {
+        console.log(message);
+    }).catch((err) => {
+    console.log(err);
+});
+
+const fs=require("fs")
+const fs = require("fs");
+// Asynchronous file read example
+fs.readFile("filepath","utf-8",(err,data)=>{
+    if(err) return console.log(err)
+    console.log(data);
+})
--- Lec 5/index.js
+++ Lec 5/index.js
@@ -1,59 +1,40 @@
-// function buyproduct(product_name,cb){
-//     //do some asynchronous operation
-//     setTimeout(()=>{
-//         //all the operation completed
-//         console.log("all the IO operation is completed and order details are written in user data")
-//         cb();
-//     })
-// }
 
-// buyproduct("Iphone 15",function(){
-//     console.log("product is purchased")
-// })
-let product=[{
-    name:"samsung",
-    amount:70000,
-    quantity:10
-},
-{
-    name:"iPhone 16",
-    amount:100000,
-    quantity:0
-}
+let users=[
+    {
+        id:1,
+        name:"Tushya",
+        age:20
+    },
+    {
+        id:2,
+        name:"abc",
+        age:16
+    }
 ]
-function buyproduct(product_name,cb){
-    //do some asynchronous operation
-    let isProduct=product.filter((p)=>p.name==product_name)[0]
-    if(!isProduct){
-        return cb("product is not available",null)
-    }
-    cb(null,isProduct.amount)
-}
-let availableamount=10000000
-function deductbankamount(amount,cb){
-    ///do some bank transactions
-    if(amount>availableamount){
-        return cb("bank balance is low",null)
-    }else{
-        availableamount-=amount
-        console.log(availableamount)
-        cb(null,"amount deducted")
-        cb(null,"amount deducted")
-        
-    }
+
+function isAllowed(id){
+    return new Promise((resolve, reject) => {
+        let user = users.filter(user => user.id == id)[0];
+        if (user) {
+            if (user.age >= 18) {
+                resolve(user.name + " is eligible to vote.");
+            } else {
+                reject(user.name + " is not eligible to vote.");
+            }
+        } else {
+            reject("User not found.");
+        }
+    });
 }
 
-buyproduct("samsung",function(err,amount){
-    if(err) return console.log(err)
-    console.log(amount);
-    deductbankamount(amount,function(err,message){
-        if(err) return console.log(err)
-            console.log(message)
-    })
-})
+isAllowed(1).then((data)=> {
+    console.log(data);
+}).catch((err) => {
+    console.log(err);
+});
 
-const fs=require("fs")
-
-fs.readFile("filepath","utf-8",(err,data)=>{
-
-})
+isAllowed(2).then((data)=> {
+    console.log(data);
+}).catch((err) => {
+    console.log(err);
+}); 
